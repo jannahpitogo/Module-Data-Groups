1a90fd213515e65b80e7c0ead2cc4d96e821efd0
jannahpitogo <pitogojannah@outlook.com>
2026-01-16 00:52:36 +0100

    made test cases to fulfill the goals of the problem and to test the dedupe function.

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,14 @@
-function dedupe() {}
+function dedupe(inputArray) {
+  if (!Array.isArray(inputArray)) return [];
+
+  let notDuplicated = [];
+
+  for (let i = 0; i < inputArray.length; i++) {
+    if (!notDuplicated.includes(inputArray[i])) {
+      notDuplicated.push(inputArray[i]);
+    }
+  }
+  return notDuplicated;
+}
+
+module.exports = dedupe;

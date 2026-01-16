aa463323c468923dcd6bc06b5a8196d47bd2c9f8
jannahpitogo <pitogojannah@outlook.com>
2026-01-16 16:21:19 +0100

    Fixed the function so that it will pass through the acceptance criteria

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,17 @@
 function sum(elements) {
+  if (elements.length === 0) return 0; // empty return 0
+  if (elements.length === 1) return elements[0]; // If one number only it will rerturn the same
+  if (elements.every((item) => typeof item !== "number")) return -Infinity; //all non number return -Infinity
+
+  let result = 0;
+
+  elements = elements.filter((item) => typeof item == "number");
+
+  for (let i = 0; i < elements.length; i++) {
+    result = result + elements[i]; //adding value
+  }
+
+  return result;
 }
 
 module.exports = sum;

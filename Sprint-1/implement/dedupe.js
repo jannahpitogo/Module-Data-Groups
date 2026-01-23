4d5430724ad24cb6859ccf76b3ad73081b845ce5
jannahpitogo <pitogojannah@outlook.com>
2026-01-23 03:42:02 +0100

    I have changed my function code from using a loop into a Set for a more efficient way of coding.

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,14 +1,7 @@
 function dedupe(inputArray) {
   if (!Array.isArray(inputArray)) return [];
 
-  let notDuplicated = [];
-
-  for (let i = 0; i < inputArray.length; i++) {
-    if (!notDuplicated.includes(inputArray[i])) {
-      notDuplicated.push(inputArray[i]);
-    }
-  }
-  return notDuplicated;
+  return [...new Set(inputArray)];
 }
 
 module.exports = dedupe;

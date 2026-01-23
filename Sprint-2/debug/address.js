c5f76b4d4192c29600f6a393246c61891852c9ad
jannahpitogo <pitogojannah@outlook.com>
2026-01-23 04:00:07 +0100

    Explained and fix the houseNumber using dot notation.

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -12,4 +12,5 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`); // It won't show the houseNumber because it's using a notation for array,
+// Where in the "address" is an object.

fd7ce1ee922ce459d255ac9a057741299bf058a6
Jannah Pitogo <pitogojannah@outlook.com>
2026-01-26 03:17:37 +0100

    Did some function for converting the input value to seconds, minutes, and hours

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,19 +1,39 @@
+let hourFloat = inputNumber / 3600;
+let hours = Math.floor(hourFloat);
+let hoursRemainder = hourFloat - hours;
+let minutesFloat = hoursRemainder * 60;
+let minutes = Math.floor(minutesFloat);
+let minutesRemainder = minutesFloat - minutes;
+let seconds = Math.floor(minutesRemainder * 60);
+
 function setAlarm() {
-  const inputNumber = document.getElementById("alarmSet").value; //input field
+  const inputNumber = Number(document.getElementById("alarmSet").value); //input field
   const timeRemainingOutput = document.getElementById("timeRemaining"); //text of time remaining
-  timeRemainingOutput.textContent = `Time Remaining ${inputNumber}`;
 }
 
-function getTime(clock) {
-  const hours = Math.floor(inputNumber / 3600);
-  const hoursRemainder = inputNumber - hours;
-  const minutes = Math.floor(hoursRemainder * 60);
-  const minutesRemainder = hoursRemainder - minutes;
-  const seconds = Math.floor(minutesRemainder * 60);
-  const secondsRemainder = minutesRemainder * 60;
-  // if (hours.length > 1) { To continue, making an if for dispaying the numbers. If it's more than 2 digits of hours u need to display it and if 1 only you have to add 0
-  // }
-  const time = `hours`;
+function convert() {
+  if (seconds === 0) {
+    minutes = minutes - 1;
+    seconds = seconds + 60;
+  }
+  if (minutes === 0) {
+    hours = hours - 1;
+    minutes = minutes + 60;
+  }
+  if ((seconds, minutes, hours === 0)) {
+    clearInterval;
+  }
+
+  let remainingTime = `${hours}:${minutes}:${seconds}`;
+  return remainingTime;
+}
+
+const interval = setInterval(() => {
+  timeRemainingOutput.textContent = remainingTime;
+}, 1000);
+
+function timeInterval() {
+  inputNumber;
 }
 
 // DO NOT EDIT BELOW HERE

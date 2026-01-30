99b036e30627dd32f856b84a6c9851ddfae64b3d
Jannah Pitogo <pitogojannah@outlook.com>
2026-01-30 02:59:53 +0100

    added some images for the carousel

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,12 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/nature1.jpg",
+  "./assets/nature2.jpg",
+  "./assets/nature3.jpg",
+  "./assets/nature4.jpg",
+  "./assets/nature5.jpg",
+  "./assets/nature6.jpg",
+  "./assets/nature7.jpg",
+  "./assets/nature8.jpg",
 ];
-
 
 // Write your code here

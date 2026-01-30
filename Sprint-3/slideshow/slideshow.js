5bbc08b762c33873a56b6a6c358b0d2afd5cb61d
jannahpitogo <pitogojannah@outlook.com>
2026-01-30 14:38:49 +0100

    Added some functions and forward function, fixed some html and css

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,12 +1,57 @@
 const images = [
-  "./assets/nature1.jpg",
-  "./assets/nature2.jpg",
-  "./assets/nature3.jpg",
-  "./assets/nature4.jpg",
-  "./assets/nature5.jpg",
-  "./assets/nature6.jpg",
-  "./assets/nature7.jpg",
-  "./assets/nature8.jpg",
+  {
+    url: "./assets/nature1.jpg",
+    alt: "A-lake-pic",
+  },
+  {
+    url: "./assets/nature2.jpg",
+    alt: "a-lake-with-boats-and-mountain",
+  },
+  {
+    url: "./assets/nature3.jpg",
+    alt: "a-river-valley-with-fog",
+  },
+  {
+    url: "./assets/nature4.jpg",
+    alt: "a-terrain-with-mountains-and trees",
+  },
+  {
+    url: "./assets/nature5.jpg",
+    alt: "an-all-green-terrain",
+  },
+  {
+    url: "./assets/nature6.jpg",
+    alt: "a-lake-with-a-tree-in-the-center-and-mountains",
+  },
+  {
+    url: "./assets/nature7.jpg",
+    alt: "a-cabin-on-a-lake-foggy",
+  },
+  {
+    url: "./assets/nature8.jpg",
+    alt: "a-lake-with-terrain-and-alps-view",
+  },
 ];
 
-// Write your code here
+//FORWARD
+let imgCounter = 0;
+let imageIndex;
+if (imgCounter < images.length) {
+  imgCounter++;
+  imageIndex = images[imgCounter];
+} else {
+  imgCounter = 0;
+}
+
+function changeImgForward() {
+  const imageForward = document.getElementById("carousel-img");
+  imageForward.innerHTML = `<img src="${imageIndex}" alt="cat-pic" />`;
+}
+
+function setup() {
+  document.getElementById("forward-btn").addEventListener("click", () => {
+    changeImgForward();
+  });
+}
+
+window.onload = setup;
